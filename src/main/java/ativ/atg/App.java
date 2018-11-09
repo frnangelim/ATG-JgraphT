73599631973c2c541d1c2c7049abdb0cb46a5bd9
Giuseppe <giuseppe.mongiovi@ccc.ufcg.edu.br>
2018-11-08 22:03:02 -0300

    question 2 finished

--- src/main/java/ativ/atg/App.java
+++ src/main/java/ativ/atg/App.java
@@ -7,20 +7,23 @@ import org.jgrapht.graph.*;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 
 public class App {
-	private static final String Q1_CSV_FILE_PATH = "/home/francisco/atg/resource/nodes.csv";
+	private static final String Q1_CSV_FILE_PATH = "/home/francisco/atg/resource/q1/nodes.csv";
+	private static final String Q2_CSV_FILE_PATH = "/home/francisco/atg/resource/q2/nodes.csv";
     
     public static void main(String[] args) throws IOException {
-    	SimpleWeightedGraph<MusicQ1, DefaultWeightedEdge> graph = q1();  
-    	System.out.println(graph.vertexSet().size());
+    	SimpleWeightedGraph<MusicQ1, DefaultWeightedEdge> graphQ1 = q1();  
+    	getTop5(graphQ1);
+    	
+    	SimpleWeightedGraph<Playlist, DefaultWeightedEdge> graphQ2 = q2();  
+    	recomendPlaylisyBySong(graphQ2);
     }
-   
+
     
 	@SuppressWarnings("deprecation")
 	public static SimpleWeightedGraph q1() throws IOException {
@@ -41,7 +44,39 @@ public class App {
 		return graph;	   
 	}
 	
-	private void getTop5(SimpleWeightedGraph<MusicQ1, DefaultWeightedEdge> graph) {
+	@SuppressWarnings("deprecation")
+	public static SimpleWeightedGraph q2() throws IOException {
+		CSVReader reader = new CSVReader(new FileReader(Q2_CSV_FILE_PATH), ',');
+		
+		HeaderColumnNameMappingStrategy<Playlist> beanStrategy = new HeaderColumnNameMappingStrategy<Playlist>();
+		beanStrategy.setType(Playlist.class);
+		
+		CsvToBean<Playlist> csvToBean = new CsvToBean<Playlist>();
+		List<Playlist> playlists = csvToBean.parse(beanStrategy, reader);
+		
+		SimpleWeightedGraph<Playlist, DefaultWeightedEdge> graph = new SimpleWeightedGraph<Playlist, DefaultWeightedEdge>(DefaultWeightedEdge.class); 
+		for (Playlist playlist : playlists) {
+			graph.addVertex(playlist);
+		}
+		reader.close();
+		
+		return graph;	   
+	}
+	
+	public static void recomendPlaylisyBySong(SimpleWeightedGraph<Playlist, DefaultWeightedEdge> graph) {
+		Playlist recomendedPlaylist = null;
+		
+		for(Playlist playlist : graph.vertexSet()) {
+			if(recomendedPlaylist==null) 
+				recomendedPlaylist = playlist;
+			else if(playlist.getWeight() > recomendedPlaylist.getWeight()) {
+				recomendedPlaylist = playlist;
+			}
+		}
+		System.out.println(recomendedPlaylist.toString());
+	}
+	
+	public static void getTop5(SimpleWeightedGraph<MusicQ1, DefaultWeightedEdge> graph) {
 		ArrayList<MusicQ1> top5 = new ArrayList<>();
     	for(MusicQ1 v : graph.vertexSet()) {
     		int currentWeight = v.getWeight();
